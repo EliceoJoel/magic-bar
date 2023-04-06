4d53c9bf21f151253d85f3fdf1d8fc265e726f15
Eliceo Joel <eliceoherbas@gmail.com>
2023-04-05 23:13:09 -0400

    Moving the positionof sign in and sing up modal buttons and adding content in all catalog page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { FaCocktail } from "react-icons/fa";
 import { FiLogOut, FiSettings } from "react-icons/fi";
 import {
@@ -16,9 +16,11 @@ interface OpenDrawer {
 }
 
 function Navbar({ openDrawer }: OpenDrawer) {
+	//I will simulate the user login info here for now, it will be replace after.
+	const userInfo = { isLogged: false };
 	return (
-		<div className="navbar bg-base-200 h-16">
-			<div className="navbar-start">
+		<div className="navbar bg-base-200 h-16 px-4">
+			<div className="navbar-start w-1/6 md:w-1/4">
 				<label
 					htmlFor="theDrawer"
 					tabIndex={0}
@@ -39,54 +41,73 @@ function Navbar({ openDrawer }: OpenDrawer) {
 					<span className="uppercase">Magic Bar</span>
 				</Link>
 			</div>
-			<div className="navbar-end">
+			<div className="navbar-end w-5/6 md:w-3/4 gap-2">
 				<button className="btn btn-circle btn-ghost">
 					<HiOutlineMoon className="w-6 h-6" />
 				</button>
-				<label
-					htmlFor="theDrawer"
-					tabIndex={0}
-					className="btn btn-circle btn-ghost relative"
-					onClick={() =>
-						openDrawer({
-							position: "drawer-end",
-							content: <Cart />,
-						})
-					}
-				>
-					<HiOutlineShoppingBag className="w-6 h-6" />
-					<div className="badge badge-primary absolute badge-xs top-2 right-1"></div>
-				</label>
-				<div className="dropdown dropdown-end dropdown-hover">
-					<label tabIndex={0} className="btn btn-ghost btn-circle">
-						<div className="indicator">
-							<HiOutlineUserCircle className="h-6 w-6" />
+				{userInfo.isLogged ? (
+					<>
+						<label
+							htmlFor="theDrawer"
+							tabIndex={0}
+							className="btn btn-circle btn-ghost relative"
+							onClick={() =>
+								openDrawer({
+									position: "drawer-end",
+									content: <Cart />,
+								})
+							}
+						>
+							<HiOutlineShoppingBag className="w-6 h-6" />
+							<div className="badge badge-primary absolute badge-xs top-2 right-1"></div>
+						</label>
+						<div className="dropdown dropdown-end dropdown-hover">
+							<label tabIndex={0} className="btn btn-ghost btn-circle">
+								<div className="indicator">
+									<HiOutlineUserCircle className="h-6 w-6" />
+								</div>
+							</label>
+							<ul
+								tabIndex={0}
+								className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+							>
+								<li>
+									<a>
+										<HiOutlineUserCircle className="w-6 h-6" />
+										Profile
+									</a>
+								</li>
+								<li>
+									<a>
+										<FiSettings className="w-6 h-6" />
+										Settings
+									</a>
+								</li>
+								<li>
+									<a>
+										<FiLogOut className="w-6 h-6" />
+										Logout
+									</a>
+								</li>
+							</ul>
 						</div>
-					</label>
-					<ul
-						tabIndex={0}
-						className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-					>
-						<li>
-							<a>
-								<HiOutlineUserCircle className="w-6 h-6" />
-								Profile
-							</a>
-						</li>
-						<li>
-							<a>
-								<FiSettings className="w-6 h-6" />
-								Settings
-							</a>
-						</li>
-						<li>
-							<a>
-								<FiLogOut className="w-6 h-6" />
-								Logout
-							</a>
-						</li>
-					</ul>
-				</div>
+					</>
+				) : (
+					<div className="flex gap-2">
+						<label
+							htmlFor="signInModal"
+							className="btn btn-primary btn-outline btn-sm capitalize"
+						>
+							Sign in
+						</label>
+						<label
+							htmlFor="signUpModal"
+							className="btn btn-outline btn-sm capitalize"
+						>
+							Sign up
+						</label>
+					</div>
+				)}
 			</div>
 		</div>
 	);
