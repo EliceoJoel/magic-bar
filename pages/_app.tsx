d790ee74a98cfa6feb38cc3c53318943083708d7
Eliceo Joel <eliceoherbas@gmail.com>
2023-04-26 15:49:12 -0400

    26 testing client id functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,12 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-	const environment = process.env.NODE_ENV;
-	const googleOAuthID =
-		environment == "production"
-			? process.env.GOOGLE_OAUTH_ID
-			: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_ID;
-  console.log(googleOAuthID);
+	let googleOAuthID = process.env.GOOGLE_OAUTH_ID ?? "";
+  if (process.env.NODE_ENV == "development") {
+    googleOAuthID = process.env.NEXT_PUBLIC_GOOGLE_OAUTH_ID ?? "";
+  }
 	return (
-		<GoogleOAuthProvider clientId={googleOAuthID ?? ""}>
+		<GoogleOAuthProvider clientId={googleOAuthID}>
 			<Component className="bg-base-200" {...pageProps} />
 		</GoogleOAuthProvider>
 	);
