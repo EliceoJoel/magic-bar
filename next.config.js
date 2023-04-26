55f84c431e3d72f5a0d8e67e2008b45727063b0e
Eliceo Joel <eliceoherbas@gmail.com>
2023-04-26 16:01:25 -0400

    26 Refactoring code and just create one env variable for google oauth id

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	env: {
+		GOOGLE_OAUTH_ID: process.env.GOOGLE_OAUTH_ID,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,8 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-	let googleOAuthID = process.env.GOOGLE_OAUTH_ID ?? "";
-  if (process.env.NODE_ENV == "development") {
-    googleOAuthID = process.env.NEXT_PUBLIC_GOOGLE_OAUTH_ID ?? "";
-  }
+	const googleOAuthID = process.env.GOOGLE_OAUTH_ID ?? "";
+	
 	return (
 		<GoogleOAuthProvider clientId={googleOAuthID}>
 			<Component className="bg-base-200" {...pageProps} />
